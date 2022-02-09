33784b9e12e9e051f044521bf58e3c7167767eed
Bohdan Oleksiuk <lordapos@gmail.com>
2022-02-09 13:47:15 +0200

    getProducts

--- graphql/resolver/productResolver.js
+++ graphql/resolver/productResolver.js
@@ -1,21 +1,50 @@
 const Product = require('../../models/Product');
+const Sequelize = require('sequelize');
+const Category = require('../../models/Category');
+const SubTaxonomy = require('../../models/SubTaxonomy');
+const Op = Sequelize.Op;
 
 module.exports = {
   Query: {
-    async getProducts() {
+    async getProducts(root, {categoryId, subTaxonomy, limit, offset}) {
       try {
-        const tmp = await Product.findAll({
+        let args = {};
+        let category = {};
+        let filters = {};
+        if (categoryId) {
+          category = {
+            id: categoryId,
+          };
+        }
+        if (subTaxonomy) {
+          filters = {
+            id: {
+              [Op.or]: subTaxonomy,
+            },
+          };
+        }
+        args = {
           order: [
             ['createdAt', 'DESC'],
           ],
           include: [
             {
+              model: Category,
+              where: category,
+            },
+            {
+              model: SubTaxonomy,
+              where: filters,
+            },
+            {
               all: true,
             },
           ],
-        });
-        console.log(tmp);
-        return tmp
+          limit: limit,
+          offset: offset,
+          distinct: true,
+        };
+        return await Product.findAndCountAll({args});
       } catch (e) {
         throw new Error('Fetch products is not available');
       }
--- graphql/schema.js
+++ graphql/schema.js
@@ -13,9 +13,44 @@ module.exports = buildSchema(`
       youtubeEmbed: String
       rating: Int
       numReviews: Int
+      category: [Category]
+      subTaxonomies: [SubTaxonomy]
+      galleries: [Gallery]
+      highlights: [Highlight]
+      reviews: [Review]
+      specs: [Spec]
+    }
+    
+    type CountProd {
+      rows: [Product]
+      count: Int
+    }
+    
+    type Category {
+      id: Int
+    }
+    
+    type SubTaxonomy {
+      id: Int
+    }
+    
+    type Gallery {
+      id: Int
+    }
+    
+    type Highlight {
+      id: Int
+    }
+    
+    type Review {
+      id: Int
+    }
+    
+    type Spec {
+      id: Int
     }
     
     type Query {
-      getProducts: [Product]
+      getProducts(categoryId: Int, subTaxonomy: [Int], limit: Int, offset: Int): CountProd
     }
 `)
