4271aef872f3db515ff2f094f3b70bdba228f9c8
Bohdan Oleksiuk <lordapos@gmail.com>
2022-02-09 14:19:22 +0200

    fix getProducts

--- graphql/resolver/productResolver.js
+++ graphql/resolver/productResolver.js
@@ -24,7 +24,7 @@ module.exports = {
         }
         return await Product.findAndCountAll({
           order: [
-            ['id', 'DESC'],
+            ['createdAt', 'DESC'],
           ],
           include: [
             {
